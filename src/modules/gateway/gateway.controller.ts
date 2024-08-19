c33bcb503b0726aaf8dff9b5f2ec384d5790cb0d
magdy <mohamedmagdycivily@gmail.com>
2024-08-19 03:17:34 +0300

    feat: adding swagger

--- src/modules/gateway/gateway.controller.ts
+++ src/modules/gateway/gateway.controller.ts
@@ -1,26 +1,77 @@
-import { Controller, Post, Body, Param, Get } from '@nestjs/common';
+import { Controller, Post, Body, Param, Get, NotAcceptableException } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiParam, ApiBody } from '@nestjs/swagger';
 import { GatewayService } from './gateway.service';
-import { CreateGatewayDto, PeripheralDto } from './dto/gateway.dto';
+import { CreateGatewayDto } from './dto/gateway.dto';
 import { Gateway } from './entity/gateway.entity';
 import { ObjectId } from 'mongodb';
 
+@ApiTags('Gateways')
 @Controller('gateways')
 export class GatewayController {
   constructor(private readonly gatewayService: GatewayService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new gateway' })
+  @ApiBody({
+    description: 'Data for the new gateway',
+    type: CreateGatewayDto,
+    examples: {
+      example1: {
+        summary: 'A basic gateway',
+        value: {
+          name: 'Main Gateway',
+          ipAddress: '192.168.1.1',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Gateway created successfully',
+    type: Gateway,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid data provided',
+  })
   async create(@Body() createGatewayDto: CreateGatewayDto): Promise<Gateway> {
     return this.gatewayService.create(createGatewayDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all gateways' })
+  @ApiResponse({
+    status: 200,
+    description: 'List of all gateways',
+    type: [Gateway],
+  })
   async findAll(): Promise<Gateway[]> {
     return this.gatewayService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a gateway by ID' })
+  @ApiParam({
+    name: 'id',
+    description: 'Object ID of the gateway',
+    type: String,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Gateway found by ID',
+    type: Gateway,
+  })
+  @ApiResponse({
+    status: 406,
+    description: 'Not Acceptable - wrong object id',
+  })
   async findOneById(@Param('id') id: string): Promise<Gateway> {
-    const objectId = new ObjectId(id);  // Convert the string to ObjectId
+    let objectId; 
+    try {
+      objectId = new ObjectId(id);  // Convert the string to ObjectId
+    } catch (error) {
+      throw new NotAcceptableException('wrong object id');
+    }
     return this.gatewayService.findOneById(objectId);
   }
 }
--- src/modules/peripheral/peripheral.controller.ts
+++ src/modules/peripheral/peripheral.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Param, Body, Put, Delete, Patch } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body, Put, Delete, Patch, NotAcceptableException } from '@nestjs/common';
 import { PeripheralService } from './peripheral.service';
 import { ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { PeripheralDto } from './dto/peripheral.dto';
@@ -12,34 +12,116 @@ export class PeripheralController {
   constructor(private readonly peripheralService: PeripheralService) {}
 
   @Post('/')
+  @ApiOperation({ summary: 'Create a new peripheral' })
+  @ApiBody({
+    description: 'Data for the new peripheral',
+    type: PeripheralDto,
+    examples: {
+      example1: {
+        summary: 'A basic peripheral',
+        value: {
+          "uid": 1234,
+          "vendor": "TV"
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Peripheral created successfully',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid data provided',
+  })
   async createPeripheral(
     @Body() peripheralDto: PeripheralDto,
   ): Promise<any> {
     return this.peripheralService.createPeripheral(peripheralDto);
   }
 
-  @Post(':idPeripheral/gateway/:idGateway')
+  @Patch(':idPeripheral/gateway/:idGateway')
+  @ApiOperation({ summary: 'Add a peripheral to a gateway' })
+  @ApiParam({
+    name: 'idGateway',
+    description: 'Object ID of the gateway to which the peripheral will be added',
+    type: String,
+  })
+  @ApiParam({
+    name: 'idPeripheral',
+    description: 'Object ID of the peripheral to be added',
+    type: String,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Peripheral added to the gateway successfully',
+    type: Gateway,
+  })
+  @ApiResponse({
+    status: 406,
+    description: 'Not Acceptable - wrong object id',
+  })
   async addPeripheral(
     @Param('idGateway') idGateway: string,
     @Param('idPeripheral') idPeripheral: string,
   ): Promise<Partial<Gateway>> {
-    return this.peripheralService.addPeripheral(new ObjectId(idGateway), new ObjectId(idPeripheral));
+    let objectIdGateway;
+    let objectIdPeripheral;
+    try{
+      objectIdGateway = new ObjectId(idGateway);  // Convert the string to ObjectId
+      objectIdPeripheral = new ObjectId(idPeripheral);  // Convert the string to ObjectId
+    }catch(error){
+      throw new NotAcceptableException("wrong object id");
+    }
+    return this.peripheralService.addPeripheral(objectIdGateway, objectIdPeripheral);
   }
 
   @Patch(':idPeripheral')
+  @ApiOperation({ summary: 'Remove a peripheral from a gateway' })
+  @ApiParam({
+    name: 'idPeripheral',
+    description: 'Object ID of the peripheral to be removed',
+    type: String,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'The gateway with the peripheral removed',
+    type: Gateway,
+  })
+  @ApiResponse({
+    status: 406,
+    description: 'Not Acceptable - wrong object id',
+  })
   async removePeripheral(
     @Param('idPeripheral') idPeripheral: string,
   ): Promise<Partial<Gateway>> {
-    return this.peripheralService.removePeripheral(new ObjectId(idPeripheral));
+    let  objectId;
+    try{
+      objectId = new ObjectId(idPeripheral);  // Convert the string to ObjectId
+    }catch(error){
+      throw new NotAcceptableException("wrong object id");
+    }
+    return this.peripheralService.removePeripheral(objectId);
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a peripheral by ID' })
+  @ApiParam({ name: 'id', description: 'Object ID of the peripheral', type: String })
+  @ApiResponse({ status: 200, description: 'Peripheral found', type: Peripheral })
+  @ApiResponse({ status: 404, description: 'Peripheral not found' })
   async findOneById(@Param('id') id: string): Promise<Peripheral | null> {
-    const objectId = new ObjectId(id);  // Convert the string to ObjectId
+    let  objectId;
+    try{
+      objectId = new ObjectId(id);  // Convert the string to ObjectId
+    }catch(error){
+      throw new NotAcceptableException("wrong object id");
+    }
     return this.peripheralService.findOneById(objectId);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all peripherals' })
+  @ApiResponse({ status: 200, description: 'Array of peripherals', type: [Peripheral] })
   async findAll(): Promise<Peripheral[]> {
     return this.peripheralService.findAll();
   }
